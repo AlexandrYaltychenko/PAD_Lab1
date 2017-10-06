d325411abc9ff50e2fa0a6499293ea69323db338
alexandr <sashaialt95@gmail.com>
2017-10-06 10:33:26 +0300

    readme fix

--- src/main/kotlin/protocol/Protocol.kt
+++ src/main/kotlin/protocol/Protocol.kt
@@ -4,7 +4,6 @@ object Protocol {
     val PORT_NUMBER = 14141
     val HOST = "127.0.0.1"
     val DEFAULT_ROUTE = ""
-    val IDLE_MESSAGE = "IDLE"
     val CRON_INTERVAL: Long = 5000
     val CRON_DELAY: Long = 5000
     val CLIENT_INTERVAL: Long = 1000
